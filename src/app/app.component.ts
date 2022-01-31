82bb9a1b80b598bf6d0695b4b21f46af8165448e
lopesleandro.it <lopesleandro.it@outlook.com>
2022-01-30 22:00:15 -0300

    adicionado os observebles user e openTasksCount necessários ao componente user-area

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { ProjectService } from './project/project.service';
 import { Project } from './shared/models/project.interface';
+import { Task } from './shared/models/task.interface';
+import { User } from './shared/models/user.interface';
+import { TaskService } from './tasks/task.service';
+import { UserService } from './user/user.service';
 
 @Component({
   selector: 'tks-root',
@@ -11,10 +16,22 @@ import { Project } from './shared/models/project.interface';
 export class AppComponent {
   projects: Observable<Project[]>;
   selectedProject: Observable<Project | undefined>;
+  openTasksCount!: Observable<number>;
+  user!: Observable<User>
 
-  constructor(private projectService: ProjectService) {
+  constructor(
+    private projectService: ProjectService,
+    private taskListService: TaskService,
+    private userService: UserService) {
+
     this.projects = projectService.getProjects();
     this.selectedProject = this.projectService.getSelectedProject();
+    this.openTasksCount = this.taskListService.getTasks().pipe(
+      map((tasks: Task[]) => {
+        return tasks.filter((task) => !task.done).length;
+      })
+    );
+    this.user = this.userService.getCurrentUser();
   }
 
   public selectProject(id: number): void {
